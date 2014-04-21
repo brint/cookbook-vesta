351d35b00844f851974be8264fc87c1175bc0c53
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-04-21 13:27:31 -0500

    Version bump to 0.1.2 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'brint.ohearn@rackspace.com'
 license          'Apache 2.0'
 description      'Installs Vesta control panel'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 recipe           'vesta', 'Installs Vesta'
 recipe           'vesta::firewall', 'Opens Vesta management port'
 
